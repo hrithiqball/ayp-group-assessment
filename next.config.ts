360966803c42635cbaa835638aa7107d4a0ae494
Harith Iqbal <harith.iqbal@swiftlogistics.com.my>
2025-06-11 10:53:59 +0800

    gh-pages cicd

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/ayp-group-assessment',
+  basePath: process.env.PAGES_BASE_PATH,
   trailingSlash: true,
 }
 
